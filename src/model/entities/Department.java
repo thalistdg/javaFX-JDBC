613e4269d86a756ca5417c581bd56c920270e85b
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-08-12 18:01:54 -0300

    acessing database

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,4 +1,3 @@
-
 package model.entities;
 
 import java.io.Serializable;
@@ -6,22 +5,23 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	
-	public Department() {}
+	public Department() {
+	}
 
 	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -37,7 +37,7 @@ public class Department implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -50,7 +50,10 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
@@ -59,6 +62,4 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-	
-	
 }
